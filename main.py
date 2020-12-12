85970fe2265e22fdbc7db88f06e73ca76fdff9cc
Brian Whiteley <brianwhiteleybw@gmail.com>
2020-12-11 23:09:46 -0600

    first section

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
 import tkinter
 
+window = tkinter.Tk()
+window.title("My First GUI Program")
+window.minsize(width=500, height=300)
+
+
+my_label = tkinter.Label(text="I am a label", font=("Arial", 24))
+my_label.pack(side="bottom")
+
+
+
+
+
+
+window.mainloop()
+
