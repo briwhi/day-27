caae58b00ab44e9f4ebe0df5662bf3941e41f0a5
Brian Whiteley <brianwhiteleybw@gmail.com>
2020-12-12 23:01:46 -0600

    tkinter basics

--- main.py
+++ main.py
@@ -1,29 +1,41 @@
 from tkinter import *
 
-window = Tk()
-window.title("My First GUI Program")
-window.minsize(width=500, height=300)
-
-
-my_label = Label(text="I am a label", font=("Arial", 24))
-my_label.pack()
-text_label = ''
 
 def button_clicked():
     print("I got clicked")
-    my_label["text"] = input.get()
+    new_text = input.get()
+    my_label.config(text=new_text)
 
-# Entry
+
+window = Tk()
+window.title("My First GUI Program")
+window.minsize(width=500, height=300)
+window.config(padx=100, pady=200)
+
+#Label
+my_label = Label(text="I Am a Label", font=("Arial", 24, "bold"))
+my_label.config(text="New Text")
+my_label.grid(column=0, row=0)
+my_label.config(padx=50, pady=50)
+
+#Button
+button = Button(text="Click Me", command=button_clicked)
+button.grid(column=1, row=1)
+
+new_button = Button(text="New Button")
+new_button.grid(column=2, row=0)
+
+#Entry
 input = Entry(width=10)
-input.pack()
+print(input.get())
+input.grid(column=3, row=2)
 
 
-button = Button(text = "Click Me", command=button_clicked)
-button.pack()
+
+
 
 
 
 
 
 window.mainloop()
-
